a6977afcd2a394291c66a5fec36a6dc9cd535292
ytkj <y.kojima.1989@gmail.com>
2019-06-10 21:20:16 +0900

    enter optional values to setup py.

--- setup.py
+++ setup.py
@@ -15,6 +15,13 @@ extras_require = {
 
 setuptools.setup(
     name='sqlalchemy_ag_grid',
+    version='0.0.1',
+    describtion='SQLAlchemy Query class suitable for AgGrid request',
+    url='https://github.com/ytkj/sqlalchemy-ag-grid',
+    author='ytkj',
+    author_email='y.kojima.1989@gmail.com',
+    license='MIT',
+    keywords='Flask SQLAlchemy AgGrid',
     install_requires=install_requires,
     test_requires=extras_require['test'],
     extras_require=extras_require,
@@ -23,5 +30,8 @@ setuptools.setup(
     },
     packages=[
         'sqlalchemy_ag_grid',
-    ]
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
 )
